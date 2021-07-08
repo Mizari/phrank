b184c28813c66d7248400a9b85e377e24e43ec14
Mizari <gomizari@gmail.com>
2021-07-08 12:54:02 +0700

    bugfix

--- phrank.py
+++ phrank.py
@@ -5,7 +5,6 @@ import os
 idaapi.require("phrank_func")
 idaapi.require("phrank_cpp")
 idaapi.require("phrank_containers")
-idaapi.require("phrank_varmap")
 idaapi.require("phrank_hexrays")
 idaapi.require("phrank_util")
 import phrank_cpp
