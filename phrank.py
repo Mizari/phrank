0018fa769ee4b1d05163eacb8c1de82e1e20a8df
Mizari <gomizari@gmail.com>
2021-11-12 19:45:26 +0700

    implemented action to create vtable by hand

--- phrank.py
+++ phrank.py
@@ -7,6 +7,7 @@ idaapi.require("phrank_util")
 
 import phrank_cpp
 import phrank_containers
+import phrank_hexrays
 
 def analyze_everything():
 	phrank_cpp.CppClassFactory().analyze_everything()
@@ -22,3 +23,46 @@ def create_cpp_vtables():
 
 def create_vtables():
 	phrank_containers.VtableFactory().create_all_vtables()
+
+class VtableMaker(idaapi.action_handler_t):
+	def __init__(self):
+		super().__init__()
+		self.factory = phrank_containers.VtableFactory()
+
+	def activate(self, ctx):
+		hx_view = idaapi.get_widget_vdui(ctx.widget)
+		cfunc = hx_view.cfunc
+		citem = hx_view.item
+		if citem.citype != idaapi.VDI_EXPR:
+			return 0
+
+		expr = citem.it.to_specific_type
+
+		parent_asg = expr
+		while parent_asg is not None:
+			if parent_asg.op == idaapi.cot_asg:
+				break
+			parent_asg = cfunc.body.find_parent_of(parent_asg).to_specific_type
+
+		if parent_asg is None:
+			return
+
+		intval = phrank_hexrays.get_int(parent_asg.y)
+		if intval is None:
+			return 0
+
+		vtbl = self.factory.create_vtable(addr=intval)
+		if vtbl is None:
+			print("failed to create vtable at", hex(intval))
+		else:
+			print("successfully created vtable", vtbl.get_name(), "at", hex(intval))
+		return 1
+
+	def update(self, ctx):
+		if ctx.widget_type == idaapi.BWN_PSEUDOCODE:
+			return idaapi.AST_ENABLE_FOR_WIDGET
+		return idaapi.AST_DISABLE_FOR_WIDGET
+
+idaapi.register_action(
+	idaapi.action_desc_t("vtblmkr", "make vtable", VtableMaker(), "Alt-Q")
+)
