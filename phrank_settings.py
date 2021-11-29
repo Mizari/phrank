76481522ee27d634f3c09c9959b748a04f608622
Mizari <gomizari@gmail.com>
2021-11-29 18:56:16 +0700

    even more prefixes

--- phrank_settings.py
+++ phrank_settings.py
@@ -4,4 +4,10 @@ FUNCTION_PREFIXES_DECOMPILATION_SKIP_LIST = [
 	"std::",
 	"boost::",
 	"spdlog::",
+	"fmt::",
+	"operator delete",
+	"operator new",
+	"__gnu_internal::",
+	"__cxxabiv1::",
+	"__gnu_cxx::",
 ]
