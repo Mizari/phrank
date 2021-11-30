89195a40b2d77812bb188e5772c2a8aff8b95a0c
Mizari <gomizari@gmail.com>
2021-11-30 15:05:51 +0700

    commit cleanup

--- phrank_func.py
+++ phrank_func.py
@@ -192,7 +192,6 @@ def set_func_argvar_type(addr, arg_id, var_type):
 	return f.set_argvar_type(arg_id, var_type)
 
 def decompile(addr):
-	print("decompiling", hex(addr), idaapi.get_name(addr))
 	f = FuncWrapper.create(addr=addr, noraise=True)
 	if f is None:
 		raise BaseException("No such function")
