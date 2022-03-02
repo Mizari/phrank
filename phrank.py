c065c112d4c395e57e3c2cf20c8dadc9bd4689d4
Mizari <gomizari@gmail.com>
2022-03-02 20:58:58 +0700

    moved useful functions to api

--- phrank.py
+++ phrank.py
@@ -1,76 +1,19 @@
 import idaapi
-import idc
+
 idaapi.require("phrank.phrank_func")
 idaapi.require("phrank.phrank_cpp")
 idaapi.require("phrank.phrank_containers")
 idaapi.require("phrank.phrank_hexrays")
 idaapi.require("phrank.phrank_util")
 idaapi.require("phrank.phrank_settings")
+idaapi.require("phrank_api")
 
-import phrank.phrank_cpp as phrank_cpp
-import phrank.phrank_containers as phrank_containers
-import phrank.phrank_hexrays as phrank_hexrays
-import phrank.phrank_util as phrank_util
-import phrank.phrank_func as phrank_func
-import phrank.phrank_settings as phrank_settings
+import phrank_api
 
-import time
-
-def analyze_everything():
-	phrank_cpp.CppClassFactory().analyze_everything()
-
-def analyze_func(addr):
-	phrank_cpp.CppClassFactory().analyze_func(addr)
-
-def analyze_vtable(addr):
-	phrank_cpp.CppClassFactory().analyze_vtable(addr)
-
-def create_cpp_vtables():
-	phrank_cpp.CppVtableFactory().create_all_vtables()
-
-def create_vtables():
-	phrank_containers.VtableFactory().create_all_vtables()
-
-def should_skip_decompiling(func_wrapper):
-	fname = func_wrapper.get_name()
-	if fname is None:
-		print("emtpy name %s" % hex(func_wrapper.get_start()))
-		return True
-
-	if phrank_settings.should_skip_by_prefix(fname):
-		return True
-
-	# global constructors
-	if fname.startswith("_GLOBAL__sub_I_"):
-		return True
-
-	dfname = idaapi.demangle_name(fname, idaapi.MNG_NODEFINIT | idaapi.MNG_NORETTYPE)
-	if dfname is not None and phrank_settings.should_skip_by_prefix(dfname):
-		return True
-
-	return False
-
-def decompile_wrappers(fwrappers):
-	time_amount = time.time()
-	for fw in fwrappers:
-		fw.decompile()
-	time_amount = time.time() - time_amount
-
-	print("decompiling", len(fwrappers), "took", round(time_amount, 3))
-
-def decompile_all():
-	fwrappers = [phrank_func.FuncWrapper(addr=fea) for fea in phrank_util.iterate_all_functions()]
-	fwrappers = filter(None, fwrappers)
-	fwrappers = filter(lambda x: not should_skip_decompiling(x), fwrappers)
-	fwrappers = list(fwrappers)
-	phrank_settings.DECOMPILE_RECURSIVELY = True
-	decompile_wrappers(fwrappers)
-	phrank_settings.DECOMPILE_RECURSIVELY = False
 
 class VtableMaker(idaapi.action_handler_t):
 	def __init__(self):
 		super().__init__()
-		self.factory = phrank_containers.VtableFactory()
 
 	def activate(self, ctx):
 		if ctx.widget_type != idaapi.BWN_PSEUDOCODE:
@@ -79,25 +22,12 @@ class VtableMaker(idaapi.action_handler_t):
 		hx_view = idaapi.get_widget_vdui(ctx.widget)
 		cfunc = hx_view.cfunc
 		citem = hx_view.item
-		if citem.citype != idaapi.VDI_EXPR:
+		intval = phrank_api.citem_to_int(cfunc, citem)
+		if intval == idaapi.BADADDR:
+			print("Failed to get int value")
 			return 0
 
-		expr = citem.it.to_specific_type
-
-		parent_asg = expr
-		while parent_asg is not None:
-			if parent_asg.op == idaapi.cot_asg:
-				break
-			parent_asg = cfunc.body.find_parent_of(parent_asg).to_specific_type
-
-		if parent_asg is None:
-			return
-
-		intval = phrank_hexrays.get_int(parent_asg.y)
-		if intval is None:
-			return 0
-
-		vtbl = self.factory.create_vtable(addr=intval)
+		vtbl = phrank_api.create_vtable(intval)
 		if vtbl is None:
 			print("failed to create vtable at", hex(intval))
 		else:
---
+++ phrank_api.py
@@ -0,0 +1,87 @@
+import idaapi
+
+import phrank.phrank_cpp as phrank_cpp
+import phrank.phrank_util as phrank_util
+import phrank.phrank_func as phrank_func
+import phrank.phrank_settings as phrank_settings
+import phrank.phrank_containers as phrank_containers
+import phrank.phrank_hexrays as phrank_hexrays
+
+import time
+
+def analyze_everything():
+	phrank_cpp.CppClassFactory().analyze_everything()
+
+def analyze_func(addr):
+	phrank_cpp.CppClassFactory().analyze_func(addr)
+
+def analyze_vtable(addr):
+	phrank_cpp.CppClassFactory().analyze_vtable(addr)
+
+def create_cpp_vtables():
+	phrank_cpp.CppVtableFactory().create_all_vtables()
+
+def create_vtables():
+	phrank_containers.VtableFactory().create_all_vtables()
+
+def create_vtable(addr):
+	factory = phrank_containers.VtableFactory()
+	return factory.create_vtable(addr=addr)
+
+def citem_to_int(cfunc, citem):
+	if citem.citype != idaapi.VDI_EXPR:
+		return idaapi.BADADDR
+
+	expr = citem.it.to_specific_type
+
+	parent_asg = expr
+	while parent_asg is not None:
+		if parent_asg.op == idaapi.cot_asg:
+			break
+		parent_asg = cfunc.body.find_parent_of(parent_asg).to_specific_type
+
+	if parent_asg is None:
+		return idaapi.BADADDR
+
+	intval = phrank_hexrays.get_int(parent_asg.y)
+	if intval is None:
+		return idaapi.BADADDR
+
+	return intval
+
+def should_skip_decompiling(func_wrapper):
+	fname = func_wrapper.get_name()
+	if fname is None:
+		print("emtpy name %s" % hex(func_wrapper.get_start()))
+		return True
+
+	if phrank_settings.should_skip_by_prefix(fname):
+		return True
+
+	# global constructors
+	if fname.startswith("_GLOBAL__sub_I_"):
+		return True
+
+	dfname = idaapi.demangle_name(fname, idaapi.MNG_NODEFINIT | idaapi.MNG_NORETTYPE)
+	if dfname is not None and phrank_settings.should_skip_by_prefix(dfname):
+		return True
+
+	return False
+
+def decompile_wrappers(fwrappers):
+	time_amount = time.time()
+	for fw in fwrappers:
+		fw.decompile()
+	time_amount = time.time() - time_amount
+
+	print("decompiling", len(fwrappers), "took", round(time_amount, 3))
+
+def decompile_all():
+	fwrappers = [phrank_func.FuncWrapper(addr=fea) for fea in phrank_util.iterate_all_functions()]
+	fwrappers = filter(None, fwrappers)
+	fwrappers = filter(lambda x: not should_skip_decompiling(x), fwrappers)
+	fwrappers = list(fwrappers)
+	phrank_settings.DECOMPILE_RECURSIVELY = True
+	decompile_wrappers(fwrappers)
+	phrank_settings.DECOMPILE_RECURSIVELY = False
+
