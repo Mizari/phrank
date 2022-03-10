ae379f79fda89f2a9f0d8a0d44a87b456978338e
Mizari <gomizari@gmail.com>
2022-03-10 16:03:36 +0700

    bugfix

--- phrank/phrank_cpp.py
+++ phrank/phrank_cpp.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import idaapi
 import idautils
 import idc
@@ -6,7 +8,6 @@ import phrank.phrank_util as p_util
 import phrank.phrank_hexrays as p_hrays
 import phrank.phrank_containers as p_cont
 
-from __future__ import annotations
 from typing import Optional
 
 class CppVtable(p_cont.Vtable):
