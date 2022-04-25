be812c9c260c5c5c0130da0ad282f58daa538ffd
Mizari <gomizari@gmail.com>
2022-04-25 18:06:54 +0700

    added descriptions

--- phrank_api.py
+++ phrank_api.py
@@ -19,22 +19,37 @@ def _analysis_api(func):
 
 @_analysis_api
 def analyze_everything():
+	"""
+	Starts analysis with all virtual tables. Then proceeds to analyze all functions from them.
+	"""
 	phrank_cpp.CppClassFactory().analyze_everything()
 
 @_analysis_api
 def analyze_func(addr):
+	"""
+	Does a C++ analysis of a function.
+	"""
 	phrank_cpp.CppClassFactory().analyze_func(addr)
 
 @_analysis_api
 def analyze_vtable(addr):
+	"""
+	Does a C++ analysis of a virtual table.
+	"""
 	phrank_cpp.CppClassFactory().analyze_vtable(addr)
 
 @_analysis_api
 def analyze_variable(cfunc, var):
+	"""
+	Analyzes a memory pointer in a variable.
+	"""
 	struct_analysis.StructFactory().analyze_variable(cfunc, var)
 
 @_analysis_api
 def create_cpp_vtables():
+	"""
+	Creates C++ virtual tables in data segment
+	"""
 	phrank_cpp.CppVtableFactory().create_all_vtables()
 
 @_analysis_api
@@ -42,10 +57,17 @@ def create_vtables():
 	phrank_containers.VtableFactory().create_all_vtables()
 
 def create_vtable(addr):
+	"""
+	Creates a virtual table at given address.
+	"""
 	factory = phrank_containers.VtableFactory()
 	return factory.create_vtable(addr=addr)
 
+@_analysis_api
 def decompile_all():
+	"""
+	Decompiles all functions in the database recursively.
+	"""
 	fwrappers = [phrank_func.FuncWrapper(addr=fea) for fea in phrank_util.iterate_all_functions()]
 	fwrappers = filter(None, fwrappers)
 	fwrappers = filter(lambda x: not x.should_skip_decompiling(), fwrappers)
