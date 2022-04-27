6c899409059615f57eaa17198a27315b7530fadb
Mizari <gomizari@gmail.com>
2022-04-27 16:05:31 +0700

    new feature

--- phrank/phrank_struct_analysis.py
+++ phrank/phrank_struct_analysis.py
@@ -5,7 +5,7 @@ class StructFactory:
 	def __init__(self):
 		return
 
-	def analyze_variable(self, cfunc, var_id):
+	def analyze_variable(self, cfunc, var_id, force_new_type=False):
 		fuv : p_hrays.FuncAnalysisVisitor = p_hrays.FuncAnalysisVisitor.create(addr=cfunc.entry_ea)
 		func = fuv.get_func()
 		func.set_cfunc(cfunc)
@@ -14,16 +14,18 @@ class StructFactory:
 		if var_size == 0:
 			return
 
-		tif = var.tif
-		if tif.is_ptr():
-			tif = tif.get_pointed_object()
+		current_type = var.tif
+		if current_type.is_ptr():
+			current_type = current_type.get_pointed_object()
 
-		if tif.is_struct():
-			pass
+		if current_type.is_struct() and not force_new_type:
+			current_struct = p_containers.Struct(name=str(current_type))
+			if current_struct.get_size() < var_size:
+				current_struct.resize(var_size)
+		else:
+			new_struct = p_containers.Struct()
+			new_struct.resize(var_size)
+			new_struct_tif = new_struct.get_tinfo()
+			new_struct_tif.create_ptr(new_struct_tif)
 
-		new_struct = p_containers.Struct()
-		new_struct.resize(var_size)
-		new_struct_tif = new_struct.get_tinfo()
-		new_struct_tif.create_ptr(new_struct_tif)
-
-		func.set_var_type(var_id, new_struct_tif)
+			func.set_var_type(var_id, new_struct_tif)
