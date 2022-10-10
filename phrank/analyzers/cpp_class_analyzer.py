be812f2c73aa30c87e585b6eff429b068a896d38
Mizari <gomizari@gmail.com>
2022-10-10 18:14:09 +0700

    more slots removal

--- phrank/analyzers/cpp_class_analyzer.py
+++ phrank/analyzers/cpp_class_analyzer.py
@@ -51,8 +51,6 @@ class ClassConstructionContext(TypeAnalyzer):
 
 
 class CppClassAnalyzer(object):
-	__slots__ = "_created_classes", "_created_unions",\
-		"_original_func_types", "_cctx", "user_ctors", "user_dtors"
 	__instance = None
 
 	def __new__(cls, *args, **kwargs):
