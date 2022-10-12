98daa1094870c52f1a5f8b4413a807206a996f6f
Mizari <gomizari@gmail.com>
2022-10-12 12:38:01 +0700

    expanded interface a bit

--- phrank/analyzers/type_analyzer.py
+++ phrank/analyzers/type_analyzer.py
@@ -3,18 +3,47 @@
 
 class TypeAnalyzer:
 	def __init__(self) -> None:
+		# analysis context
+		# type analysis
 		self.type2func = {}
 		self.type2reads = {}
 		self.type2writes = {}
 
+		# analysis context
+		# analyzed types without actually changing types
 		self.lvar2type = {}
 		self.gvar2type = {}
 		self.field2type = {}
 		self.retval2type = {}
 
-		self.new_types = []
-		self.new_xrefs = []
-		self.new_lvars = []
-		self.new_gvars = []
-		self.new_fields = []
-		self.new_retvals = []
+		# analysis results
+		self.new_types = []    # created types
+		self.new_xrefs = []    # created xrefs
+		self.new_lvars = []    # changed types of local variables
+		self.new_gvars = []    # changed types of global variables
+		self.new_fields = []   # changed types of struct fields
+		self.new_retvals = []  # changed types of function return values
+
+	def clear_analysis(self):
+		return
+
+	def apply_analysis(self):
+		return
+
+	def analyze_everything(self):
+		return
+
+	def analyze_function(self):
+		return
+
+	def analyze_lvar(self):
+		return
+
+	def analyze_gvar(self):
+		return
+
+	def analyze_cexpr(self):
+		return
+
+	def analyze_field(self):
+		return
