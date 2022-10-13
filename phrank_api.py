656f8ec58c1d765fc5487c038afc270ccf4c5add
Mizari <gomizari@gmail.com>
2022-10-13 11:33:43 +0700

    no more singletons and multitons

--- phrank_api.py
+++ phrank_api.py
@@ -9,54 +9,37 @@ from phrank.analyzers.vtable_analyzer import VtableAnalyzer
 from phrank.analyzers.cpp_vtable_analyzer import CppVtableAnalyzer
 from phrank.analyzers.cpp_class_analyzer import CppClassAnalyzer
 
-def _analysis_api(func):
-	@functools.wraps(func)
-	def fwrapper(*args, should_clear_cache=True, **kwargs):
-		rv = func(*args, **kwargs)
-		if should_clear_cache:
-			util_func.FuncWrapper.clear_cached_instances()
-			util_ast.ASTAnalysis.clear_cached_instances()
-			VtableAnalyzer().clear_created_vtables()
-			CppVtableAnalyzer().clear_created_vtables()
-		return rv
-	return fwrapper
 
-@_analysis_api
 def analyze_everything():
 	"""
 	Starts analysis with all virtual tables. Then proceeds to analyze all functions from them.
 	"""
 	CppClassAnalyzer().analyze_everything()
 
-@_analysis_api
 def analyze_func(addr):
 	"""
 	Does a C++ analysis of a function.
 	"""
 	CppClassAnalyzer().analyze_func(addr)
 
-@_analysis_api
 def analyze_vtable(addr):
 	"""
 	Does a C++ analysis of a virtual table.
 	"""
 	CppClassAnalyzer().analyze_vtable(addr)
 
-@_analysis_api
 def analyze_variable(cfunc, var):
 	"""
 	Analyzes a memory pointer in a variable.
 	"""
 	StructAnalyzer().analyze_variable(cfunc, var)
 
-@_analysis_api
 def create_cpp_vtables():
 	"""
 	Creates C++ virtual tables in data segment
 	"""
 	CppVtableAnalyzer().create_all_vtables()
 
-@_analysis_api
 def create_vtables():
 	VtableAnalyzer().create_all_vtables()
 
@@ -67,7 +50,6 @@ def create_vtable(addr):
 	factory = VtableAnalyzer()
 	return factory.create_vtable(addr=addr)
 
-@_analysis_api
 def decompile_all():
 	"""
 	Decompiles all functions in the database recursively.
