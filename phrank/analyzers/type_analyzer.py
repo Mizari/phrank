60e2cdbe1d57ac38c7679cde55e21a53674bb01e
Mizari <gomizari@gmail.com>
2022-10-14 17:20:43 +0700

    better interface

--- phrank/analyzers/type_analyzer.py
+++ phrank/analyzers/type_analyzer.py
@@ -48,6 +48,7 @@ class TypeAnalyzer:
 		return aa
 
 	def clear_analysis(self):
+		# delete temporaly created new types
 		for t in self.new_types:
 			t.delete()
 		self.new_types.clear()
@@ -59,17 +60,17 @@ class TypeAnalyzer:
 	def analyze_everything(self):
 		raise NotImplementedError
 
-	def analyze_function(self):
+	def analyze_function(self, func_ea):
 		raise NotImplementedError
 
-	def analyze_lvar(self):
+	def analyze_lvar(self, func_ea, lvar_id):
 		raise NotImplementedError
 
-	def analyze_gvar(self):
+	def analyze_gvar(self, gvar_ea):
 		raise NotImplementedError
 
-	def analyze_cexpr(self):
+	def analyze_cexpr(self, cfunc, cexpr):
 		raise NotImplementedError
 
-	def analyze_field(self):
+	def analyze_field(self, struct, offset):
 		raise NotImplementedError
