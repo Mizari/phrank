8f26706a8614ceb01ebac0d49324da07cbce021d
Mizari <gomizari@gmail.com>
2022-10-25 15:38:26 +0700

    refactoring

--- phrank/analyzers/struct_analyzer.py
+++ phrank/analyzers/struct_analyzer.py
@@ -6,19 +6,23 @@ class StructAnalyzer(TypeAnalyzer):
 	def __init__(self, func_factory=None) -> None:
 		super().__init__(func_factory=func_factory)
 
-	def analyze_variable(self, cfunc, var_id, force_new_type=False):
-		fuv = self.get_ast_analysis(cfunc.entry_ea)
-		var_size = fuv.get_var_use_size(var_id)
+	def analyze_lvar(self, func_ea, lvar_id):
+		cfunc = self.get_cfunc(func_ea)
+		if cfunc is None:
+			return
+
+		fuv = self.get_ast_analysis(func_ea)
+		var_size = fuv.get_var_use_size(lvar_id)
 		if var_size == 0:
 			return
 
-		var_type = self.get_var_type(var_id)
-		if force_new_type or var_type is None:
+		var_type = self.get_var_type(lvar_id)
+		if var_type is None:
 			new_struct = Structure()
 			new_struct.resize(var_size)
 			new_struct_tif = new_struct.get_tinfo()
 			new_struct_tif.create_ptr(new_struct_tif)
-			self.set_var_type(var_id, new_struct_tif)
+			self.set_var_type(lvar_id, new_struct_tif)
 			return
 
 		if var_type is None:
