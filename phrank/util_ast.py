85b3aefc6c44683abbc27ef53575b776b54c19d4
Mizari <gomizari@gmail.com>
2022-11-01 13:35:05 +0700

    refactoring: better code separation by logic

--- phrank/util_ast.py
+++ phrank/util_ast.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 import idaapi
 
+import phrank.util_aux as util_aux
 
 ARRAY_FUNCS = {"qmemcpy", "memcpy", "strncpy", "memset", "memmove", "strncat", "strncmp"}
 ARRAY_FUNCS.update(['_' + s for s in ARRAY_FUNCS])
@@ -119,3 +122,168 @@ def get_int(expr):
 		return expr.n._value
 
 	return None
+
+
+class Write:
+	def __init__(self, val):
+		self.val : idaapi.cexpr_t|None = val
+
+	def is_int(self, val=None):
+		intval = get_int(self.val)
+		if intval is None:
+			return False
+
+		if val is None:
+			return True
+
+		return val == intval
+
+	def get_write_size(self):
+		sz = self.val.type.get_size()
+		if sz == idaapi.BADSIZE:
+			raise BaseException("Failed to get write size " + self.val.opname)
+		return sz
+
+	def check_val(self, val):
+		if isinstance(val, int):
+			return self.is_int(val)
+		return self.val == val
+
+class VarAccess:
+	def __init__(self, varid, offset):
+		self.varid = varid
+		self.offset = offset
+
+	def get_var_use(self, var_id):
+		if self.varid != var_id:
+			return 0
+		else:
+			return self.offset
+
+class VarWrite(Write):
+	def __init__(self, varid, val):
+		super().__init__(val)
+		self.varid = varid
+
+	def check(self, val=None):
+		if val is not None and not self.check_val(val):
+			return False
+		return True
+
+class VarPtrWrite(Write):
+	def __init__(self, varid, val, offset):
+		super().__init__(val)
+		self.varid = varid
+		self.offset : int|None = offset
+
+	def get_int(self):
+		return get_int(self.val)
+	
+	def get_var_use(self, var_id):
+		if self.varid != var_id:
+			return 0
+		return self.offset + self.get_write_size()
+
+	def check(self, offset=None, val=None):
+		if offset is not None and self.offset != offset:
+			return False
+
+		if val is not None and not self.check_val(val):
+			return False
+		return True
+
+class FuncCall:
+	def __init__(self, call_expr):
+		self._call_expr : idaapi.cexpr_t = call_expr
+		self._func_ea : int = idaapi.BADADDR
+		self._func_name : str|None = None
+
+		if call_expr.x.op == idaapi.cot_obj:
+			self._func_ea = call_expr.x.obj_ea
+			self._func_name = idaapi.get_func_name(self._func_ea)
+		elif call_expr.x.op == idaapi.cot_helper:
+			self._func_name = call_expr.x.helper
+
+		self._this_args : dict[int, int] = {}
+
+	def get_ea(self):
+		if self._func_ea == idaapi.BADADDR:
+			return None
+		return self._func_ea
+
+	def get_nargs(self):
+		return len(self._call_expr.a)
+
+	def get_args(self):
+		return self._call_expr.a
+
+	def get_name(self):
+		return self._func_name
+
+	def get_var_offset(self):
+		for arg in self._call_expr.a:
+			varid, offset = get_var_offset(arg)
+			if varid == -1:
+				continue
+
+			return varid, offset
+		return None
+
+	def get_var_use_size(self, var_id):
+		nargs = self.get_nargs()
+		if nargs == 0:
+			return 0
+
+		arg0 = self._call_expr.a[0]
+		varid, offset = get_var_offset(arg0)
+		if varid == var_id:
+			func_use_value = 0
+			if self._func_name in ARRAY_FUNCS:
+				arg2 = self._call_expr.a[2]
+				if arg2.op == idaapi.cot_num:
+					func_use_value = arg2.n._value
+			elif self._func_name in WARRAY_FUNCS:
+				arg2 = self._call_expr.a[2]
+				if arg2.op == idaapi.cot_num:
+					func_use_value = arg2.n._value * 2
+			elif self._func_name in PRINTF_FUNCS:
+				arg2 = self._call_expr.a[1]
+				if arg2.op == idaapi.cot_num:
+					func_use_value = arg2.n._value
+
+			if func_use_value != 0:
+				return offset + func_use_value
+
+		# sanity check
+		if self._func_ea == idaapi.BADADDR:
+			return 0
+
+		# cant look into imported funcs, assume that args are somehow used there
+		if util_aux.is_func_import(self._func_ea):
+			return 1
+
+		if idaapi.get_func(self._func_ea) is None:
+			return 0
+
+		max_var_use = 0
+		for arg_id in range(nargs):
+			arg = self._call_expr.a[arg_id]
+			varid, offset = get_var_offset(arg)
+			if varid == -1:
+				continue
+
+			if varid != var_id:
+				continue
+
+			"""
+			fav: ASTAnalysis = ASTAnalysis.create(addr=self._func_ea)
+			var_use = fav.get_var_use_size(arg_id)
+			max_var_use = max(max_var_use, var_use + offset)
+			"""
+		return max_var_use
+
+
+class ReturnWrapper:
+	def __init__(self, insn) -> None:
+		self.insn = insn
+
