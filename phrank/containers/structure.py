9574b9e6f01ba9c06dfb0d79cdc2589acf891e0c
Mizari <gomizari@gmail.com>
2022-11-02 11:31:43 +0700

    refactored expanding, now does not do thousands  of operation on large expansions.

--- phrank/containers/structure.py
+++ phrank/containers/structure.py
@@ -14,27 +14,22 @@ class Structure(IdaStrucWrapper):
 		for member_offset in self.member_offsets():
 			yield idc.get_member_name(self.strucid, member_offset), hex(member_offset)
 
-	def resize(self, size):
-		# TODO ida_struct.expand_struc
-		if self.get_size() == size: return
+	def resize(self, new_size: int):
+		current_size = self.get_size()
+		if current_size == new_size: return
 
-		if self.get_size() > size:
-			self.unset_members(size, self.get_size() - size)
+		if current_size > new_size:
+			self.unset_members(new_size, current_size - new_size)
 			return
 
-		ptr_size = util_aux.get_ptr_size()
-		# self.size < size == True
-		fill = size % ptr_size 
-		size -= fill
-		while self.get_size() != size:
-			membername = 'field_' + hex(self.get_size())[2:]
-			if membername[-1] == 'L': membername = membername[:-1]
-			ret = idc.add_struc_member(self.strucid, membername, self.get_size(), util_aux.size2dataflags(ptr_size), -1, ptr_size)
+		self.expand(new_size - current_size)
 
-		for _ in range(fill):
-			membername = 'field_' + hex(self.get_size())[2:]
-			if membername[-1] == 'L': membername = membername[:-1]
-			ret = idc.add_struc_member(self.strucid, membername, self.get_size(), util_aux.size2dataflags(1), -1, 1)
+	# TODO ida_struct.expand_struc
+	def expand(self, extra_size: int):
+		current_size = self.get_size()
+		membername = 'field_' + hex(current_size)[2:]
+		idc.add_struc_member(self.strucid, membername, current_size, util_aux.size2dataflags(1), -1, 1)
+		idc.expand_struc(self.strucid, current_size, extra_size - 1, False)
 
 	def is_offset_ok(self, offset, size):
 		if offset + size <= self.get_size(): return True
