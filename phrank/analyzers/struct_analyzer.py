5e4e079cef877747986bd420d5a2d9104687d1af
Mizari <gomizari@gmail.com>
2022-11-25 13:38:23 +0700

    reworked lvar analysis

--- phrank/analyzers/struct_analyzer.py
+++ phrank/analyzers/struct_analyzer.py
@@ -13,6 +13,36 @@ class StructAnalyzer(TypeAnalyzer):
 		self.analyzed_functions = set()
 		self.vtable_analyzer = VtableAnalyzer(func_factory)
 
+	def get_lvar_writes(self, func_ea, lvar_id):
+		func_aa = self.get_ast_analysis(func_ea)
+		for var_write in func_aa.get_writes_into_var(lvar_id):
+			write_offset = var_write.offset
+			write_type = self.analyze_cexpr(func_ea, var_write.val)
+			# write exists, just type is unknown. will use simple int instead
+			if write_type is None:
+				write_type = utils.get_int_tinfo(var_write.val.type.get_size())
+			yield write_offset, write_type
+
+	def get_lvar_call_arg_casts(self, func_ea, lvar_id):
+		func_aa = self.get_ast_analysis(func_ea)
+		for func_call in func_aa.get_calls():
+			call_ea = func_call.get_ea()
+			for arg_id, arg in enumerate(func_call.get_args()):
+				varid, offset = utils.get_var_offset(arg)
+				if varid != lvar_id or offset == 0: continue
+
+				# if helper function, then skip
+				if call_ea is None: continue
+
+
+				arg_tinfo = self.analyze_lvar(call_ea, arg_id)
+				if arg_tinfo is None: continue
+
+				if arg_tinfo.is_ptr() and offset != 0:
+					arg_tinfo = arg_tinfo.get_pointed_object()
+
+				yield offset, arg_tinfo
+
 	def get_var_use_size(self, func_ea:int, lvar_id:int) -> int:
 		func_aa = self.get_ast_analysis(func_ea)
 		max_var_use = func_aa.get_var_use_size(lvar_id)
@@ -45,47 +75,22 @@ class StructAnalyzer(TypeAnalyzer):
 			return lvar_tinfo
 		return self.analyze_lvar(func_ea, lvar_id)
 
-	def calculate_lvar_type_usage(self, func_ea, lvar_id, new_lvar_tinfo):
-		lvar_strucid = utils.tif2strucid(new_lvar_tinfo)
-		if lvar_strucid == idaapi.BADADDR:
-			return
-
-		lvar_struct = Structure(struc_locator=lvar_strucid)
+	def calculate_lvar_type_usage(self, func_ea, lvar_id, lvar_struct: Structure):
 		var_size = self.get_var_use_size(func_ea, lvar_id)
 		lvar_struct.maximize_size(var_size)
 
-		func_aa = self.get_ast_analysis(func_ea)
-		for var_write in func_aa.get_writes_into_var(lvar_id):
-			write_offset = var_write.offset
-			write_type = self.analyze_cexpr(func_ea, var_write.val)
-			# write exists, just type is unknown. will use simple int instead
-			if write_type is None:
-				write_type = utils.get_int_tinfo(var_write.val.type.get_size())
+		for write_offset, write_type in self.get_lvar_writes(func_ea, lvar_id):
 			if not lvar_struct.member_exists(write_offset):
 				lvar_struct.add_member(write_offset)
 			lvar_struct.set_member_type(write_offset, write_type)
 
-		for func_call in func_aa.get_calls():
-			call_ea = func_call.get_ea()
-			for arg_id, arg in enumerate(func_call.get_args()):
-				varid, offset = utils.get_var_offset(arg)
-				if varid != lvar_id or offset == 0: continue
+		for offset, arg_tinfo in self.get_lvar_call_arg_casts(func_ea, lvar_id):
+			if not lvar_struct.member_exists(offset):
+				lvar_struct.add_member(offset)
+			lvar_struct.set_member_type(offset, arg_tinfo)
 
-				# if helper function, then skip
-				if call_ea is None: continue
-
-				# type offset is used, member should exist
-				if not lvar_struct.member_exists(offset):
-					lvar_struct.add_member(offset)
-
-				arg_tinfo = self.analyze_lvar(call_ea, arg_id)
-				if arg_tinfo is None: continue
-
-				if arg_tinfo.is_ptr() and offset != 0:
-					arg_tinfo = arg_tinfo.get_pointed_object()
-				lvar_struct.set_member_type(offset, arg_tinfo)
-
-	def calculate_passed_lvar_type(self, func_ea, lvar_id):
+	def calculate_lvar_type_by_uses(self, func_ea, lvar_id):
+		# TODO passed var without uses in this func_ea or with non-conflicting uses
 		func_aa = self.get_ast_analysis(func_ea)
 		offset0_lvar_passes = []
 		for func_call in func_aa.get_calls():
@@ -107,7 +112,7 @@ class StructAnalyzer(TypeAnalyzer):
 		else:
 			return None
 
-	def calculate_current_lvar_type(self, func_ea, lvar_id):
+	def calculate_new_lvar_type(self, func_ea, lvar_id, struc_tinfo):
 		func_aa = self.get_ast_analysis(func_ea)
 
 		var_type = self.get_var_type(func_ea, lvar_id)
@@ -122,19 +127,17 @@ class StructAnalyzer(TypeAnalyzer):
 				if func_aa.count_writes_into_var(lvar_id) == 0:
 					return None
 				else:
-					lvar_struct = Structure()
-					self.new_types.append(lvar_struct)
-					return lvar_struct.get_ptr_tinfo()
+					struc_tinfo.create_ptr(struc_tinfo)
+					return struc_tinfo
 
 			if pointed.is_struct():
-				return var_type
+				return struc_tinfo
 
 			elif pointed.is_void() or pointed.is_integral():
 				if func_aa.count_writes_into_var(lvar_id) == 0:
 					return None
-				lvar_struct = Structure()
-				self.new_types.append(lvar_struct)
-				return lvar_struct.get_ptr_tinfo()
+				struc_tinfo.create_ptr(struc_tinfo)
+				return struc_tinfo
 
 			else:
 				print("WARNING:", "unknown pointer tinfo", str(var_type), "in", idaapi.get_name(func_ea))
@@ -143,9 +146,7 @@ class StructAnalyzer(TypeAnalyzer):
 		elif var_type.is_void() or var_type.is_integral():
 			if func_aa.count_writes_into_var(lvar_id) == 0:
 				return None
-			lvar_struct = Structure()
-			self.new_types.append(lvar_struct)
-			return lvar_struct.get_tinfo()
+			return struc_tinfo
 
 		else:
 			print("WARNING:", "failed to create struct from tinfo", str(var_type), "in", idaapi.get_name(func_ea))
@@ -209,36 +210,48 @@ class StructAnalyzer(TypeAnalyzer):
 		print("WARNING:", "unknown assigned value in", idaapi.get_name(func_ea), "for", lvar_id)
 		return None
 
-	def calculate_lvar_type(self, func_ea, lvar_id):
-		passed_lvar_type = self.calculate_passed_lvar_type(func_ea, lvar_id)
-		if passed_lvar_type is not None:
-			return passed_lvar_type
+	def analyze_existing_lvar_type(self, func_ea, lvar_id):
+		lvar_tinfo = self.get_var_type(func_ea, lvar_id)
+		if lvar_tinfo is not None and utils.tif2strucid(lvar_tinfo) != idaapi.BADADDR:
+			# TODO check correctness of writes, read, casts
+			return lvar_tinfo
 
-		assigned_lvar_type = self.calculate_assigned_lvar_type(func_ea, lvar_id)
-		if assigned_lvar_type is not None:
-			return assigned_lvar_type
+		lvar_tinfo = self.calculate_assigned_lvar_type(func_ea, lvar_id)
+		if lvar_tinfo is None:
+			lvar_tinfo = self.calculate_lvar_type_by_uses(func_ea, lvar_id)
 
-		current_lvar_type = self.calculate_current_lvar_type(func_ea, lvar_id)
-		if current_lvar_type is not None:
-			return current_lvar_type
+		if lvar_tinfo is not None:
+			# TODO check correctness of writes, read, casts
+			pass
 
-		return None
+		return lvar_tinfo
+
+	def analyze_new_lvar_type(self, func_ea, lvar_id):
+		lvar_struct = Structure()
+		lvar_tinfo = self.calculate_new_lvar_type(func_ea, lvar_id, lvar_struct.get_tinfo())
+		if lvar_tinfo is None:
+			lvar_struct.delete()
+			return None
+		else:
+			self.new_types.append(lvar_struct.strucid)
+
+		self.calculate_lvar_type_usage(func_ea, lvar_id, lvar_struct)
+		return lvar_tinfo
 
 	def analyze_lvar(self, func_ea, lvar_id):
 		current_lvar_tinfo = self.lvar2tinfo.get((func_ea, lvar_id))
 		if current_lvar_tinfo is not None:
 			return current_lvar_tinfo
 
-		new_lvar_tinfo = self.calculate_lvar_type(func_ea, lvar_id)
-		if new_lvar_tinfo is None:
+		lvar_tinfo = self.analyze_existing_lvar_type(func_ea, lvar_id)
+		if lvar_tinfo is None:
+			lvar_tinfo = self.analyze_new_lvar_type(func_ea, lvar_id)
+
+		if lvar_tinfo is None:
 			return None
-		self.lvar2tinfo[(func_ea, lvar_id)] = new_lvar_tinfo
 
-		# calculate only complex types modifications
-		if utils.tif2strucid(new_lvar_tinfo) != idaapi.BADADDR:
-			self.calculate_lvar_type_usage(func_ea, lvar_id, new_lvar_tinfo)
-
-		return new_lvar_tinfo
+		self.lvar2tinfo[(func_ea, lvar_id)] = lvar_tinfo
+		return lvar_tinfo
 
 	def analyze_retval(self, func_ea):
 		rv = self.retval2tinfo.get(func_ea)
