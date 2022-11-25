52b6f4586290b81acd9732f28ebf9ba9497e930e
Mizari <gomizari@gmail.com>
2022-11-25 12:04:08 +0700

    added useful api

--- phrank_api.py
+++ phrank_api.py
@@ -3,5 +3,6 @@
 from phrank.analyzers.struct_analyzer import StructAnalyzer
 from phrank.analyzers.vtable_analyzer import VtableAnalyzer
 from phrank.analyzers.cpp_class_analyzer import CppClassAnalyzer
+from phrank.ast_analyzer import ASTAnalyzer
 
 from phrank.utils import *
