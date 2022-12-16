84d7d3bf62485e041a7489e7498c5854c2cc282b
Mizari <gomizari@gmail.com>
2022-12-16 21:27:37 +0700

    refactoring

--- phrank.py
+++ phrank.py
@@ -1,7 +1,6 @@
 import idaapi
 
 import phrank_api
-import phrank.utils as utils
 
 
 def get_lvar_id(cfunc, lvar_arg):
@@ -69,7 +68,7 @@ class HRActionHandler(idaapi.action_handler_t):
 
 class VtableMaker(HRActionHandler):
 	def handl_expr(self, cfunc, citem):
-		intval = utils.get_int(citem)
+		intval = phrank_api.get_int(citem)
 		if intval is None:
 			print("Failed to get int value")
 			return 0
