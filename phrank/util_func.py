2cd6a26660ef3e79ccaeaf87c3896effa9a298df
Mizari <gomizari@gmail.com>
2023-05-21 11:53:13 +0700

    bugfix

--- phrank/util_func.py
+++ phrank/util_func.py
@@ -6,8 +6,14 @@ import idautils
 import re
 
 def is_func_start(addr:int) -> bool:
-	if is_func_import(addr): return True
 	if addr == idaapi.BADADDR: return False
+
+	if is_import_addr(addr):
+		tif = idaapi.tinfo_t()
+		idaapi.get_tinfo(tif, addr)
+		if tif.is_func():
+			return True
+
 	return addr == get_func_start(addr)
 
 def get_func_start(addr:int) -> int:
@@ -74,13 +80,18 @@ def get_trampoline_func_target(func_ea:int) -> int:
 	return -1
 
 def is_func_import(func_ea:int) -> bool:
-	if idc.get_segm_name(func_ea) in (".idata", ".plt"):
+	if is_import_addr(func_ea):
 		return True
 
 	tramp_target = get_trampoline_func_target(func_ea)
 	if tramp_target != -1:
 		return is_func_import(tramp_target)
 
+	return False
+
+def is_import_addr(addr:int) -> bool:
+	if idc.get_segm_name(addr) in (".idata", ".plt"):
+		return True
 	return False
 
 def iterate_all_functions():
