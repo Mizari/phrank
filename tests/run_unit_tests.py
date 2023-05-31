d2d9d53e9ad393af98774dffa2a7b4b412f1a8e7
Mizari <gomizari@gmail.com>
2023-05-31 14:09:58 +0700

    clear temp test file by default

--- tests/run_unit_tests.py
+++ tests/run_unit_tests.py
@@ -20,6 +20,7 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--idadir", help="IDA Pro directory")
 	parser.add_argument("--plog", help="phrank log file")
+	parser.add_argument("--cleartemp", default=True)
 	args = parser.parse_args()
 
 	idadir = get_idadir(args.idadir)
@@ -30,22 +31,27 @@ def main():
 	if not os.path.exists(ida_binary):
 		die(f"Failed to find ida binary at {ida_binary} path")
 
-	phrank_log_file = args.plog
-	if phrank_log_file is None:
-		phrank_log_file = tempfile.mktemp()
-		open(phrank_log_file, 'a').close()
+	phrank_log_filename = args.plog
+	temp_filename = None
+	if phrank_log_filename is None:
+		temp_filename = tempfile.mktemp()
+		open(temp_filename, 'a').close()
+		phrank_log_filename = temp_filename
 
-	if not os.path.exists(phrank_log_file):
-		die(f"Phrank log file {phrank_log_file} does not exist")
+	if not os.path.exists(phrank_log_filename):
+		die(f"Phrank log file {phrank_log_filename} does not exist")
 
 	test_script = os.path.join(os.path.dirname(__file__), "unit_tests.py")
-	call_args = [ida_binary, "-A", "-t", "-S" + test_script + ' ' + phrank_log_file]
+	call_args = [ida_binary, "-A", "-t", "-S" + test_script + ' ' + phrank_log_filename]
 	rv = subprocess.call(call_args)
 	if rv != 0:
 		die(f"Failed to run script rv={rv}")
 
-	print(f"Successfully run unit tests. Phrank log at {phrank_log_file}:\n")
-	print(open(phrank_log_file, 'r').read())
+	print(f"Successfully run unit tests. Phrank log at {phrank_log_filename}:\n")
+	print(open(phrank_log_filename, 'r').read())
+
+	if temp_filename is not None and args.cleartemp is True:
+		os.remove(temp_filename)
 
 
 if __name__ == "__main__":
