e9b8bdd8004cce5ed44618e7b5a9cef35543d33f
Mizari <gomizari@gmail.com>
2023-08-15 12:18:50 +0700

    useful api

--- pyphrank/analysis_state.py
+++ pyphrank/analysis_state.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import idaapi
 
 from pyphrank.type_flow_graph_parts import Var
@@ -15,3 +16,23 @@ class AnalysisState:
 	def clear(self):
 		self.vars.clear()
 		self.retvals.clear()
+
+	def print_type_locations(self, needle:str|int|idaapi.tinfo_t):
+		if isinstance(needle, int):
+			strucid = needle
+		elif isinstance(needle, str):
+			strucid = utils.str2strucid(needle)
+		else:
+			strucid = utils.tif2strucid(needle)
+
+		if strucid == -1:
+			return
+
+		for var, tif in self.vars.items():
+			if utils.tif2strucid(tif) != strucid:
+				continue
+			print(f"found type in {var}")
+		for func_ea, tif in self.retvals.items():
+			if utils.tif2strucid(tif) != strucid:
+				continue
+			print(f"found type in {idaapi.get_name(func_ea)}")
