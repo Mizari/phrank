a22abfbc97db92c8cb2593b1cdc6a54ba1b4bcda
Mizari <gomizari@gmail.com>
2023-09-18 15:43:08 +0700

    bugfix

--- pyphrank/ast_analyzer.py
+++ pyphrank/ast_analyzer.py
@@ -426,10 +426,10 @@ class CTreeAnalyzer:
 		elif expr.op == idaapi.cot_idx:
 			arr = lift_reuse(expr.x)
 			idx = lift_reuse(expr.y)
-			add_expr = SExpr.create_binary_op(expr.x.ea, arr, idx)
 			if expr.x.type.is_ptr() and expr.y.type.is_integral(): # pointer arithmetics
 				i = SExpr.create_type_literal(expr.x.ea, utils.str2tif("int"))
-				add_expr = SExpr.create_binary_op(expr.x.ea, add_expr, i)
+				idx = SExpr.create_binary_op(expr.x.ea, idx, i)
+			add_expr = SExpr.create_binary_op(expr.x.ea, arr, idx)
 			ptr_expr = SExpr.create_ptr(expr.ea, add_expr)
 			type_node = Node(Node.EXPR, ptr_expr)
 
